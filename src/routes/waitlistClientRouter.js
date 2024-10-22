9c39a4ac1e88161d0449a1159b0ccd6aa49e7d0e
HuangChILun <60692423+HuangChILun@users.noreply.github.com>
2024-10-22 10:07:13 -0600

    add token and admin verification

--- src/routes/waitlistClientRouter.js
+++ src/routes/waitlistClientRouter.js
@@ -2,20 +2,23 @@
 const express = require('express');
 const router = express.Router();
 const waitlistClientController = require('../controllers/waitlistClientController');
-
+const {
+    authenticateToken,
+    authorizeAdmin,
+  } = require("../middleware/authMiddleware");
 // Create a new waitlist client
-router.post('/createWaitlistClient', waitlistClientController.createWaitlistClient);
+router.post('/createWaitlistClient',authenticateToken, waitlistClientController.createWaitlistClient, authorizeAdmin);
 
 // Get all waitlist clients
-router.get('/getAllWaitlistClient', waitlistClientController.getAllWaitlistClients);
+router.get('/getAllWaitlistClient',authenticateToken, waitlistClientController.getAllWaitlistClients, authorizeAdmin );
 
 // Get a specific waitlist client by ID
-router.get('/getWaitlistClient/:id', waitlistClientController.getWaitlistClientById);
+router.get('/getWaitlistClient/:id', authenticateToken, waitlistClientController.getWaitlistClientById, authorizeAdmin);
 
 // Update a waitlist client
-router.put('/updateWaitlistClient/:id', waitlistClientController.updateWaitlistClient);
+router.put('/updateWaitlistClient/:id',authenticateToken, waitlistClientController.updateWaitlistClient, authorizeAdmin);
 
 // Delete a waitlist client
-router.delete('/deleteWaitlistClient/:id', waitlistClientController.deleteWaitlistClient);
+router.delete('/deleteWaitlistClient/:id',authenticateToken, waitlistClientController.deleteWaitlistClient, authorizeAdmin);
 
 module.exports = router;

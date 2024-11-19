350fdf8a83f8be1094323ac6bfc923a363ef49bc
qianjun-ql <qianjunliang.ql@gmail.com>
2024-11-19 11:10:52 -0700

    edit port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const http = require('http');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 const server = http.createServer(app);
 
